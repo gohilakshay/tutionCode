695c4bb2bb4936bb2263390cbaa9e90a549deeae
leela jain <leelajain34@gmail.com>
2017-07-24 13:10:14 +0530

    db form n fee details tabs

---
+++ application/controllers/CreateNewDb.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CreateNewDb extends CI_Controller
+{
+	public function createDb()
+	{
+        $this->load->helper('form');
+        $this->load->helper('url');
+		$this->load->view('createDatabse');        //html filename
+    }  
+}
