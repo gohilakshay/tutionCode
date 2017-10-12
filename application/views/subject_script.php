d5d902cdcfa215ab5f106236a8de253a46b0ed48
gohilakshay <leoakshay95@gmail.com>
2017-10-12 15:08:03 +0530

    Validation Done

--- application/views/subject_script.php
+++ application/views/subject_script.php
@@ -1,36 +1,36 @@
-<?php 
-/*$this->load->library('session');
-$db = $this->session->userdata('db');//load db      
-$this->load->database($db);//call db
-$this->db->database;
-$query['result'] = $this->SelectData->course(); 
-$coursename = array();
-foreach($result as $value){ 
-    array_push ($coursename,$value->course_name);
-}*/
-?>
-
-<script>
-//    $(document).ready(function(){
-//        $('#course').change(function() {
-//            var j = $('#course option:selected').text();
-//            alert(j);
-//        });  
-//    });
-//       
-//    start of add faculty script
-//    var course_name = <?php echo json_encode($coursename); ?>;
-//    $('#course').on('change',function(){
-//
-//            if($.inArray($(this).val(), course_name) > -1){
-//
-//            if( $(this).val()== ab){
-//
-//                $("#subjects").show()
-//            }
-//            else{
-//                $("#subjects").hide()
-//            }
-//        }
-   // });
+<?php 
+/*$this->load->library('session');
+$db = $this->session->userdata('db');//load db      
+$this->load->database($db);//call db
+$this->db->database;
+$query['result'] = $this->SelectData->course(); 
+$coursename = array();
+foreach($result as $value){ 
+    array_push ($coursename,$value->course_name);
+}*/
+?>
+
+<script>
+//    $(document).ready(function(){
+//        $('#course').change(function() {
+//            var j = $('#course option:selected').text();
+//            alert(j);
+//        });  
+//    });
+//       
+//    start of add faculty script
+//    var course_name = <?php echo json_encode($coursename); ?>;
+//    $('#course').on('change',function(){
+//
+//            if($.inArray($(this).val(), course_name) > -1){
+//
+//            if( $(this).val()== ab){
+//
+//                $("#subjects").show()
+//            }
+//            else{
+//                $("#subjects").hide()
+//            }
+//        }
+   // });
 </script>
