b71cc9f44379d3520cb5a2c268ce6e00dba24f0f
gohilakshay <leoakshay95@gmail.com>
2017-10-14 17:21:19 +0530

    enquiry notify done

--- application/views/nav.php
+++ application/views/nav.php
@@ -1,3 +1,17 @@
+<?php 
+$sql ="SELECT * FROM enquiry";
+$query = $this->db->query($sql);
+$enquiryTotal=0;
+foreach ($query->result() as $row) {
+   $followDate = $row->followup_date;
+    $date1 = date('Y-m-d', strtotime("-1 days"));
+    $date2 = date('Y-m-d', strtotime("+1 days"));
+   if($followDate >= $date1 && $followDate <= $date2){
+       $enquiryTotal++;
+      $enquiry[] = array('followDate'=>$followDate,'name'=>$row->name,'id'=>$row->enquiry_ID);
+   }
+} 
+?>
 <div class="main-panel">
 <nav class="navbar navbar-default">
             <div class="container-fluid">
@@ -18,7 +32,6 @@
 								<p>Upload</p>
                             </a>
                         </li>
-                        
                         <li class="dropdown">
                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                     <i class="ti-wallet"></i>
@@ -38,23 +51,45 @@
                                   </li>
                               </ul>
                         </li>
+                        <li class="dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="ti-bell"></i>
+									<span class="badge"><?php echo $enquiryTotal; ?></span>
+									<b class="caret"></b>
+                              </a>
+                              <ul class="dropdown-menu">
+                                  <li style="width:350px">
+                                      <table class="table table-borderless" >
+                                          <thead>
+                                            <th><strong>Sr No.</strong></th>
+                                            <th><strong>Name</strong></th>
+                                            <th><strong>Follow Up Date</strong></th>
+                                          </thead>
+                                          <tbody>
+                                              <?php $i=1;foreach($enquiry as $value){
+                                              $enqid = $value['id']; ?>
+                                              <tr>
+                                                  <td><a href="<?php echo site_url()."/Enquiry_cont/enquiryInfo/$enqid" ?>"><?php echo $i;$i++; ?></a></td>
+                                                  <td><a href="<?php echo site_url()."/Enquiry_cont/enquiryInfo/$enqid" ?>"><?php echo $value['name'] ?></a></td>                
+                                                  <td><a href="<?php echo site_url()."/Enquiry_cont/enquiryInfo/$enqid" ?>"><?php echo $value['followDate'] ?></a></td>
+                                              </tr>
+                                              <?php } ?>
+                                          </tbody>
+                                      </table>
+                                  </li>
+                              </ul>
+                        </li>
                          <li>
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <i class="ti-user"></i>
 								<p>Profile</p>
                             </a>
                              <ul class="dropdown-menu">
-                                    <!--<div class="row">
-                                        <div class="col-md-12">
-                                            <img src="<?php echo base_url()?>assets/profile/admin.jpg" width="200" height="150" class="img-circle" style="padding-left:10px; padding-right:10px; padding-top:10px; padding-bottom:10px;">
-                                        </div>
-                                    </div>-->
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <center><b><h4><?php echo $username = $this->session->userdata('username');?></h4></b></center>
-                                        </div>
-                                    </div>
-                                 
+                                 <div class="row">
+                                     <div class="col-md-12">
+                                         <center><b><h4><?php echo $username = $this->session->userdata('username');?></h4></b></center>
+                                     </div>
+                                 </div>
                                  <center>
                                      <form action="<?php echo site_url("Home") ?>" method="post">
                                          <input type="hidden" name="signout" />
