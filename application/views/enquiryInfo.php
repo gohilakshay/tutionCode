134c776717574c1365c696079a3825a0a8ddf379
gohilakshay <leoakshay95@gmail.com>
2017-10-18 10:50:08 +0530

    Enquiry info form resolved

--- application/views/enquiryInfo.php
+++ application/views/enquiryInfo.php
@@ -47,7 +47,7 @@
      </div>
         
         <div class="row">
-            <div class="col-lg-offset-2 col-md-8 col-sm-12 col-xs-12">
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
                 <div class="card">
                     <div class="content">
                         <div class="row">
