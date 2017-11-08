8dcf70fd77b1aa5b7d6c1d2291d6464e4163c017
gohilakshay <leoakshay95@gmail.com>
2017-11-08 12:22:20 +0530

    SendsmsContent

--- application/views/sendSMSContent.php
+++ application/views/sendSMSContent.php
@@ -1,4 +1,4 @@
-<div class="content"><?php  if(!null(end($result))){$button= end($result);}  ?>
+<div class="content"><?php  if(end($result)!= null){$button= end($result);}  ?>
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12 col-sm-12">
