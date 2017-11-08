05770816cb7db8bc685aa74219b68ec08fa41de2
gohilakshay <leoakshay95@gmail.com>
2017-11-08 12:44:12 +0530

    SendsmsContent

--- application/views/sendSMSContent.php
+++ application/views/sendSMSContent.php
@@ -1,4 +1,4 @@
-<div class="content"><?php  print_r($result);$button= end($result);  ?>
+<div class="content"><?php  $button= $button2;  ?>
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12 col-sm-12">
